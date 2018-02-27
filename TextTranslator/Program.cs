243b3d5f739175c1288c6bf0ec892772d1682c22
David Zeller <david.zeller@outlook.com>
2018-02-27 10:38:48 +0100

    Test

--- TextTranslator/Program.cs
+++ TextTranslator/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace TextTranslator
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.1
         /// </summary>
         [STAThread]
         static void Main()
